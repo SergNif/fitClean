ecd60e8116d502ec2a3bb7b199eb449339ea6990
SergNif <n777@bk.ru>
2022-08-20 14:25:58 +0300

    FITclean 20 08 2 commit

---
+++ app/src/main/java/com/sergnfitness/android/fit/presentation/activity/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.sergnfitness.android.fit.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.sergnfitness.android.fit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        viewModel = ViewModelProvider(this, MainViewModelFactory(this)).get(MainViewModel::class.java)
+
+
+//        val logFragment: LoginFragment = LoginFragment()
+//        val fragment: Fragment? = supportFragmentManager.findFragmentByTag(logFragment::class.java.simpleName)
+//
+//        if (fragment != logFragment){
+//        supportFragmentManager.beginTransaction()
+//            .add(LoginFragment,"Fragment Login",LoginFragment,LoginFragment::class.java.simpleName)
+//
+    }
+}
